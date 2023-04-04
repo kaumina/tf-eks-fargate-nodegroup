f1a3f3cae61c7a0fb4ce232f9125d44257852ae5
kaumina <1788436+kaumina@users.noreply.github.com>
2023-04-04 13:45:29 -0700

    created default fargate profile

--- modules/eks_cluster/fargate.tf
+++ modules/eks_cluster/fargate.tf
@@ -1,13 +1,22 @@
-# Create default Fargate profile
-resource "aws_eks_fargate_profile" "eks_farget_main" {
+# Create Fargate profile for control plane
+resource "aws_eks_fargate_profile" "eks_fargate_main" {
   cluster_name           = aws_eks_cluster.eks.name
-  fargate_profile_name   = "eks_farget_main"
+  fargate_profile_name   = "eks_fargate_kube_system"
   pod_execution_role_arn = aws_iam_role.fargate_role.arn
   subnet_ids             = var.eks_subnet_ids
 
   selector {
     namespace = "kube-system"
   }
+
+}
+# Create default Fargate profile
+resource "aws_eks_fargate_profile" "eks_fargate_default" {
+  cluster_name           = aws_eks_cluster.eks.name
+  fargate_profile_name   = "eks_fargate_default"
+  pod_execution_role_arn = aws_iam_role.fargate_role.arn
+  subnet_ids             = var.eks_subnet_ids
+
   selector {
     namespace = "default"
   }
