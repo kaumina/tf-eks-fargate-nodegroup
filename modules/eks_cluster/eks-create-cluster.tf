b2e25ed41d9c14cda806c9017ace730b87569ecb
kaumina <1788436+kaumina@users.noreply.github.com>
2023-04-06 10:26:47 -0700

    Enabled EKS CloudWatch logs

--- modules/eks_cluster/eks-create-cluster.tf
+++ modules/eks_cluster/eks-create-cluster.tf
@@ -6,6 +6,7 @@
 resource aws_eks_cluster eks {
   name            = "${var.cluster_name}"
   role_arn        = "${aws_iam_role.eks_cluster.arn}"
+  enabled_cluster_log_types = ["api", "audit"]
   vpc_config {
     security_group_ids = var.eks_security_group_ids
     subnet_ids         = var.eks_subnet_ids
@@ -16,5 +17,13 @@ resource aws_eks_cluster eks {
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.eks_cluster_AmazonEKSServicePolicy,
+    aws_cloudwatch_log_group.cluster_cw_log_grp,
   ]
+}
+
+# Creating CloudWatch log group for enabling EKS Logs
+resource "aws_cloudwatch_log_group" "cluster_cw_log_grp" {
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = 7
+
 }
